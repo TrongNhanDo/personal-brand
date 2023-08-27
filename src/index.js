5243c6b0a0e49d1b57246fe6c22dddabbaa47fb4
dtnhan <dotrongnhan15102000@gmail.com>
2023-08-27 20:12:44 +0700

    fix: fix bug gh-pages

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
    <React.StrictMode>
-      <HashRouter>
+      <HashRouter basename={window.location.pathname || ''}>
          <App />
       </HashRouter>
    </React.StrictMode>
